d03732711121cb1191f9ef7fa38e7cf29dc4ec2f
erikwijmans <ewijmans2@gmail.com>
2016-09-27 23:39:56 -0500

    Done with local work

--- buid/placeScan/cotire/placeScan_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.8 generated file */
-/* /Users/erikwijmans/Projects/c++/buid/placeScan/cotire/placeScan_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/Users/erikwijmans/Projects/c++/buid/placeScan/cotire/placeScan_CXX_prefix.cxx"
-#endif
--- buid/scanDensity/cotire/scanDensity_CXX_unity.cxx
+++
@@ -1,7 +0,0 @@
-/* cotire.cmake 1.7.8 generated file */
-/* /Users/erikwijmans/Projects/c++/buid/scanDensity/cotire/scanDensity_CXX_unity.cxx */
-#ifdef __cplusplus
-#include "/Users/erikwijmans/Projects/c++/scanDensity/scanDensity.cpp"
-#include "/Users/erikwijmans/Projects/c++/scanDensity/3DInfo.cpp"
-#include "/Users/erikwijmans/Projects/c++/scanDensity/driver.cpp"
-#endif
